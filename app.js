07178b05e6a92316fa2d000b6f5730402fe59748
WesleyCKY <wesleychin9966@gmail.com>
2021-07-27 15:37:19 +0800

    add preload background function

--- app.js
+++ app.js
@@ -57,3 +57,47 @@ function switchSrc(element, name, backDrop){
     ev.preventDefault();
   });
 }
+
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+    console.log("Pre-loaded");
+}
+
+// replace the img cache src...
+preloadImages([
+    "assets/png/map/xizhou/xizhou/xizhou_02.png",
+    "assets/png/map/chunqiu/chunqiu/chunqiu_02.png",
+    "assets/png/map/zhanguo/zhanguo/zhanguo_02.png",
+    "assets/png/map/qin/qin/qin_02.png",
+    "assets/png/map/xihan/xihan/xihan_02.png",
+    "assets/png/map/donghan/donghan/donghan_02.png",
+    "assets/png/map/sanguo/sanguo/sanguo_02.png",
+    "assets/png/map/xijin/xijin/xijin_02.png",
+    "assets/png/map/dongjin/dongjin/dongjin_02.png",
+    "assets/png/map/nanbeichao/nanbeichao/nanbeichao.png",
+    "assets/png/map/sui/sui/sui_02.png",
+    "assets/png/map/tang/tang/tang_02.png",
+    "assets/png/map/beisong/beisong/beisong_02.png",
+    "assets/png/map/nansong/nansong/nansong_02.png",
+    "assets/png/map/yuan/yuan/yuan_02.png",
+    "assets/png/map/ming/ming/ming_02.png",
+    "assets/png/map/qing/qing/qing_02.png",
+    "assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png",
+    "assets/png/map/zhongguo/zhongguo/zhongguo.png"
+]);
